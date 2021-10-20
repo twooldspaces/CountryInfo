5e3f21d900032f5919736c63076b3a62c2bb441b
Artem <twooldspace@gmail.com>
2021-10-20 11:30:18 +0500

    Initial Commit

---
+++ Country Info/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Country Info
+//
+//  Created by Artem on 20.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
